2c806b207c0c70e9c95763598877bec07494d875
Sergey Bolotov <sbol@etersoft.ru>
2015-07-30 15:59:06 +0300

    added myLogTrace functions

--- opennx/mylog.h
+++ opennx/mylog.h
@@ -23,6 +23,7 @@
 #define _MYLOG_H_
 
 #include <wx/log.h>
+#include <wx/string.h>
 
 // Compatibility with patched wxWidgets 2.8.12 on Ubuntu
 #ifndef WX_ATTRIBUTE_PRINTF_2
@@ -34,6 +35,8 @@
 extern void myLogDebug(const wxChar *szFormat, ...);
 extern void myLogTrace(const wxChar *mask, const wxChar *szFormat, ...) WX_ATTRIBUTE_PRINTF_2;
 extern void myLogTrace(wxTraceMask mask, const wxChar *szFormat, ...) WX_ATTRIBUTE_PRINTF_2;
+
+extern void myLogTrace(wxString mask, const wxChar *szFormat, ...) WX_ATTRIBUTE_PRINTF_2;
 
 #endif
     // _MYLOG_H_
